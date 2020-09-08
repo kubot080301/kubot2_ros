ae511669eba2075b8dcf4e91645af67d9a051245
Hong <49848440+Fizz080301@users.noreply.github.com>
2020-09-09 00:25:24 +0800

    Update Arduino_Nano.ino

--- ros_ws/src/kubot_led/arduino/Arduino_Nano/Arduino_Nano.ino
+++ ros_ws/src/kubot_led/arduino/Arduino_Nano/Arduino_Nano.ino
@@ -1,24 +1,24 @@
-#include <Adafruit_NeoPixel.h>      // 載入lib
+#include <Adafruit_NeoPixel.h>            // 載入lib
 
-#define PIN_ 5                      // 定義腳位，輸出腳
+#define PIN_ 5                            // 定義腳位，輸出腳
 
-int num=30;                         // LED燈珠的數量
+int num=30;                               // LED燈珠的數量
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(num, PIN_, NEO_GRB + NEO_KHZ800); 
 //Adafruit_NeoPixel類別的函式，分別為(燈珠數, 硬體連接的腳為, LED型號顏色順序與發佈頻率)
 
 int i,r,g,b,d,s_pos,e_pos;
 
-String inputString = "";            // 用於保存傳入數據的字串
-boolean stringComplete = false;     // 字串是否完整
+String inputString = "";                  // 用於保存傳入數據的字串
+boolean stringComplete = false;           // 字串是否完整
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin (115200);            // 需與上位一致
+  Serial.begin (115200);                  // 需與上位一致
   Serial.print ("Arduino Ready...\n");
   strip.begin();
-  for(int i=0;i<num;i++) {          // 點亮第i顆LED
-    strip.setPixelColor(i, 255, 60, 0 ); // 定義初始顏色 GRB色碼
+  for(int i=0;i<num;i++) {                // 點亮第i顆LED
+    strip.setPixelColor(i, 255, 60, 0 );  // 定義初始顏色 GRB色碼
   }
   strip.show();
   i=0;
